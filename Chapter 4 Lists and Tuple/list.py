662687698c35af6ca748ddb012b9db56fc508f48
aakashdeep2310 <rjaakashdeep007@gmail.com>
2025-02-14 01:03:05 +0530

    Chapter 4 code

---
+++ Chapter 4 Lists and Tuple/list.py
@@ -0,0 +1,8 @@
+friends= ["Apple", "Aakash", 5, 354.342, False] #We can add any type of data in a list
+print(friends)
+
+print(friends[1])
+
+friends[0] = "Blood" #Unlike strings, lists are mutable
+print(friends)
+print(friends[0:4]) #Slicing a list
---
+++ Chapter 4 Lists and Tuple/listMethods.py
@@ -0,0 +1,18 @@
+friends= ["Apple", "Aakash", 5, 354.342, False] #We can add any type of data in a list
+print(friends)
+
+friends.append("Blood") #Adds an element at the end of the list
+print(friends)
+
+friends.reverse() #Reverses the list
+print(friends)
+
+friends.insert(1, "Candy") #Inserts an element at a specific index
+print(friends)
+
+friends.remove("Apple") #Removes the first occurrence of the element
+print(friends)
+
+l1 = [5, 7, 3, 2, 1, 4]
+l1.sort() #Sorts the list
+print(l1)
