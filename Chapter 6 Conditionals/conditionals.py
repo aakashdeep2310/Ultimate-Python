4b1fdb05cb8211671745c0d14f85adbe232c64f4
aakashdeep2310 <rjaakashdeep007@gmail.com>
2025-02-18 01:50:55 +0530

    Chapter 6

---
+++ Chapter 6 Conditionals/conditionals.py
@@ -0,0 +1,10 @@
+
+
+age = int(input("Enter your age: "))
+
+if(age >= 18):
+    print("You are an adult")
+elif (age > 0 and age < 18):
+    print("You are a child")
+else:
+    print("Invalid age")
